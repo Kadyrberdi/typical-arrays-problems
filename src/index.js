4385d502bd430f9c88069024e22fe6df331e6ea2
Kadyrberdi <lhojamkulyyev@gmail.com>
2020-10-02 18:12:54 +0300

    feat: add function to tap

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
+exports.min = function min(array) {
+    /*if (array) {
+        if (array === undefined || array.length === 0) {            
+            if (!array) return 0; */
+    return Math.min.apply(null, array);
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    /* if (array) {
+        if (array === undefined || array.length === 0) {
+            if (!array) return 0; */
+    return Math.max.apply(null, array);
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    /*if (array) {
+        if (array === undefined || array.length === 0) {
+            if (!array) return 0;*/
+    let result = array.reduce(function (a, b) {
+        return a + b;
+    }, 0);
+    return result / array.length;
+};
