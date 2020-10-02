75e35c365da512701e1b820ae56871fea5b97d9d
Kadyrberdi <lhojamkulyyev@gmail.com>
2020-10-02 18:45:18 +0300

    fix: fix function codes

--- src/index.js
+++ src/index.js
@@ -1,23 +1,27 @@
 exports.min = function min(array) {
-    /*if (array) {
-        if (array === undefined || array.length === 0) {            
-            if (!array) return 0; */
-    return Math.min.apply(null, array);
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        return Math.min.apply(null, array);
+    }
 };
 
 exports.max = function max(array) {
-    /* if (array) {
-        if (array === undefined || array.length === 0) {
-            if (!array) return 0; */
-    return Math.max.apply(null, array);
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        return Math.max.apply(null, array);
+    }
 };
 
 exports.avg = function avg(array) {
-    /*if (array) {
-        if (array === undefined || array.length === 0) {
-            if (!array) return 0;*/
-    let result = array.reduce(function (a, b) {
-        return a + b;
-    }, 0);
-    return result / array.length;
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        let result = array.reduce(function (a, b) {
+            return a + b;
+        }, 0);
+
+        return result / array.length;
+    }
 };
